b6031f1dda39a0051f90a8af66c1aa0ec90e9bbb
quaid <quaid@iquaid.org>
2012-08-14 07:29:10 -0700

    Update modules/ovirt_infra/manifests/init.pp
    Adding myself and my key to the list of users; understood this structure may change.

--- modules/ovirt_infra/manifests/init.pp
+++ modules/ovirt_infra/manifests/init.pp
@@ -10,4 +10,8 @@ class ovirt_infra {
   ovirt_infra::user { 'ekohl':
     key => 'AAAAB3NzaC1yc2EAAAABIwAAAQEA0oQOkgks69qJkPIZeKC/uRGDyjkHHeULCdTtzs2I3ye0W0+Dg+Bl3rkhxX0S9xKanmhensvLfSXb01i6b5Qga9A8efVEgvQzDal456ahff64w+QPJ5IUPzfGYpGJAJp9MggjrgfOnIzfgC2Zo15rszOYuEmuLTsDs72kwvFVvZJJ9DPkaPvKCvoo3aykwbmJbVl7fDWPwvIKVOF+RwXeGqH2AsMwGyOVwn0Ik3ywcZhylkmoDg4RAS1n4F6gPk1GzHVcEYGMtwgVJQSi6FfEkkqwFfxRJUE562M0XwW5Zsy4FyiUsnHPMzIdcGQ1iSMGcV9z1506dSM0b/bN1guvgw==',
   }
+
+  ovirt_infra::user { 'quaid':
+    key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCfjnxEmcpPFKjdn/swdAZkhQgVL8Ngc1AG4CLFpeiW3z2KnQoF17wnqTEe7fPDtPHTxJyvsHFEUZHetBe351e8zH2USBV3IcYnQBosTG2086TWjxCBQ2N9Zi8aXLXHnv4vNQhC0ZNRbCuJ7MseKryScnPK5sFhh9u6IyaDG7ClnWtWvmR7y8dCYLb0ZAIJtSxdGHou/7K1b07AZul3L9cvOkLjSPKrSanLgWIOUjgXKbP+s9hZuiO0T7hX1Va46hvu3fthDqs7gMRLwvSUmZKjwSzlCrm8ftNhjK1snyzkoYtgC6i/1MM62EJ32qsAvudbt5tTEiXb69hHP4cGp0Hn',
+  }
 }
