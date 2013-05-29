fbb3fd7afd79eb2d30ae0e7b173644c41c5c6a45
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2013-05-29 16:30:12 +0200

    Split users into separate classes

---
+++ modules/ovirt_infra/manifests/packages.pp
@@ -0,0 +1,6 @@
+# Packages we want on all servers
+class ovirt_infra::packages {
+  package {'vim-enhanced':
+    ensure => installed,
+  }
+}
---
+++ modules/ovirt_infra/manifests/user/quaid.pp
@@ -0,0 +1,6 @@
+# The user for Karsten Wade
+class ovirt_infra::user::quaid {
+  ovirt_infra::user { 'quaid':
+    key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCfjnxEmcpPFKjdn/swdAZkhQgVL8Ngc1AG4CLFpeiW3z2KnQoF17wnqTEe7fPDtPHTxJyvsHFEUZHetBe351e8zH2USBV3IcYnQBosTG2086TWjxCBQ2N9Zi8aXLXHnv4vNQhC0ZNRbCuJ7MseKryScnPK5sFhh9u6IyaDG7ClnWtWvmR7y8dCYLb0ZAIJtSxdGHou/7K1b07AZul3L9cvOkLjSPKrSanLgWIOUjgXKbP+s9hZuiO0T7hX1Va46hvu3fthDqs7gMRLwvSUmZKjwSzlCrm8ftNhjK1snyzkoYtgC6i/1MM62EJ32qsAvudbt5tTEiXb69hHP4cGp0Hn',
+  }
+}
