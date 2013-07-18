a9134d71a53fd9b967ddf6c7403d1e1eaf5ca22c
Kiril Nesenko <knesenko@redhat.com>
2013-07-18 16:36:00 +0300

    Fix packages names in jenkins-slave module.
    Change-Id: I1cb61f933079a1ca075d7fb1446d515f13e5af5f
    Signed-off-by: Kiril Nesenko <knesenko@redhat.com>

--- modules/ovirt_infra/manifests/jenkins-slave.pp
+++ modules/ovirt_infra/manifests/jenkins-slave.pp
@@ -1,12 +1,12 @@
 class ovirt_infra::jenkins-slave {
-  $packages = ['autoconf', 'automake', 'make', 'puppet', 'java',
-    'gettext-devel', 'python2-devel', 'java-1.7.0-openjdk-devel',
+  $packages = ['autoconf', 'automake', 'make', 'puppet',
+    'gettext-devel', 'python-devel', 'java-1.7.0-openjdk-devel',
     'python-pep8', 'python-pip', 'maven', 'wget', 'python-nose',
     'ethtool', 'pyflakes', 'python-ethtool', 'libvirt',
     'libvirt-python', 'python-pthreading', 'm2crypto', 'psmisc',
     'python-netaddr', 'maven-compiler-plugin', 'maven-enforcer-plugin',
     'maven-install-plugin', 'maven-jar-plugin', 'maven-javadoc-plugin',
-    'maven-source-plugin', 'maven-surefire-provider-junit4', 'maven-local']
+    'maven-source-plugin', 'maven-surefire-provider-junit', 'maven-local']
   package {$packages:
     ensure => installed,
   }
