8b55b36da8662971d4d58ac4d1fdb8f3864fca30
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2014-02-13 11:19:46 +0100

    Update systemd to "work" on other operatingsystems
    Change-Id: I0634d6c93fb7f82c24f325d8ad4370178bf6ee79
    Signed-off-by: Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>

--- site/ovirt_infra/manifests/jnlp.pp
+++ site/ovirt_infra/manifests/jnlp.pp
@@ -1,17 +1,21 @@
-#this class configures the jnlp systemd service
+#this class configures the jnlp service
 
 class ovirt_infra::jnlp ($secret_key = $::secret_key, $jnlpurl = $::jnlpurl) {
 
+  if $::operatingsystem == 'Fedora' {
+    file {'/lib/systemd/system/jnlp.service' :
+      ensure  => file,
+      mode    => '0600',
+      content => template('ovirt_infra/jnlp.systemd.erb'),
+    }
 
-  file {'/lib/systemd/system/jnlp.service' :
-    ensure  => file,
-    mode    => '0600',
-    content => template('ovirt_infra/jnlp.erb'),
-  }
+    service {'jnlp' :
+      ensure  => running,
+      enable  => true,
+    }
 
-  service {'jnlp' :
-    ensure  => running,
-    enable  => true,
-    require => File['/lib/systemd/system/jnlp.service'],
+    File['/lib/systemd/system/jnlp.service'] ~> Service['jnlp']
+  } else {
+    notice("Unsupported operatingsystem ${::operatingsystem}")
   }
 }
