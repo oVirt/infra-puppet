6cbc1f7993d4eb107a8b76dd39007235710d6efd
David Caro <dcaroest@redhat.com>
2014-04-07 21:06:30 +0200

    Adding mskrivanek user
    This user is meant to be added only to the jenkins slaves
    
    Change-Id: Iae41bd2369578909b7d87e3c79ca555cd346d52b
    Signed-off-by: David Caro <dcaroest@redhat.com>

---
+++ site/ovirt_infra/manifests/user/devel/mskrivanek.pp
@@ -0,0 +1,7 @@
+# The user for Michael Skrivanek
+class ovirt_infra::user::devel::mskrivanek($password = undef) {
+  ovirt_infra::user {'mskrivanek':
+    key      => 'AAAAB3NzaC1yc2EAAAABIwAAAQEAogap5Muk2LAqB7wyyNEtRHQxP/bl6PpJUdc3EVs25Rx+icoo8WMGVw3RIJczx12H6YlRTMTb3mrqwBmtwb8pZAh3Me3GYJYFxHmy7Ln27f+8Z2Gb0kjGWpTzfyEOjzuMb4khwf8wBqo/hEroc/bIe87A1LFgCHLbyCGhVYaCOd6ZoWQv+L2DC4bw/zhAn3FKuaHgRAKHn4Lr7N5NbWgWSWOhUS2d8COgTGnK0rUWvYmzE1uFJRJuzKxJNA9Z2kV/WnFWBPWCROL9Uy/IX7b6sXp+J/7Qo6hFmasm1g4LLvCJKrH63Xz5D1jVebDSgGj9H41ecVpekaBkWciDd1+1dQ==',
+    password => $password,
+  }
+}
