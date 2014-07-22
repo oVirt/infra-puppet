72c2e3c7d3981e9182305c56d26616204a9c811e
David Caro <dcaroest@redhat.com>
2014-07-22 10:25:36 +0200

    Fixed conflict issue on f20
    when installing vim-enhanced and vim-minimal is not updated it conflicts and
    fails
    
    Change-Id: Ib3accc846dd41f8d36f740f003d8039552ec54af
    Signed-off-by: David Caro <dcaroest@redhat.com>

--- site/ovirt_infra/manifests/packages.pp
+++ site/ovirt_infra/manifests/packages.pp
@@ -1,6 +1,11 @@
 # Packages we want on all servers
 class ovirt_infra::packages {
+  package {'vim-minimal':
+    ensure => latest,
+  }
   package {'vim-enhanced':
     ensure => installed,
   }
+  Package['vim-minimal']
+  -> Package['vim-enhanced']
 }
