ac4179ad74a1e86a5e5f241b1a37d1818509b5ef
Barak Korren <bkorren@redhat.com>
2015-04-05 19:21:11 +0300

    Updated eedri`s SSH public key
    Change-Id: I86f1a757371c05ba0bdcccfabb52ad3a51492ee1
    Signed-off-by: Barak Korren <bkorren@redhat.com>

--- site/ovirt_infra/manifests/user/eedri.pp
+++ site/ovirt_infra/manifests/user/eedri.pp
@@ -1,7 +1,7 @@
 # The user for Eyal Edri
 class ovirt_infra::user::eedri($password = undef) {
   ovirt_infra::user { 'eedri':
-    key      => 'AAAAB3NzaC1yc2EAAAABIwAAAQEAykXy+X1qUI/TyblF5J35A1bexPeFWj7SmzzcClS3GzQ8jEaV7AaOzbvyl2dQ8P4nh8tr2nSeT7LAFYWhIGscy6V7p5vMRr3mUzRA/E/g3r9wdmdDcPLOqfpJWiLTDlA3XQyFhJnwQopGRBSf5yzFGWFezH+rjzlwBDDN2mQkI/WuSEBh+UT/9+E7JvQBVhg2hapXszfSrrtrVniw/1TvNJEvR+wdwxCUkJWP+LZOtdbGIYQZMkmw8yMNy/fkEfxR3CLge65rDCbxqlDkqFff0VWcwd3SBXdIo4T1401kIjcPiPR9npib7Ra88QiWXIazHW05ejp+m2W136zmYmfxFw==',
+    key      => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC288XR5mM3oZNwYoUjsRTzyxzSPFT+MxFwOmIOAhhOGmLHDQi0lDWbSmIwXFo17Ph5A88ucl1Iu0uHZAJ7RSImjHejZCNEcwLmB6/ytVVWacDAKgZk6eEL/2dIqREqHub2X19wIMfFgrQ1aC2C6B52IdOB8O8TbBIOD5V6wqTwgj+rPosCgI10KBsWcpS0vjQT5RPi2jizlRLDj4y6lOvaMH70vaX6ZU0w1HzANK751jWMgrYFt9eKiLcKA+YcRDqJ9WNVwZgrwHelxiUNbEgsz6pv402c8pPZGx4C8L3OvlCJ86S9tpm7kx1R11SJZbxvV/KUV85t7+Bkp8Pj4RYF'
     password => $password,
   }
 }
