b109a4f5d250be6cbc313b71c1858a9b1a47f489
Sagi Shnaidman <sshnaidm@redhat.com>
2015-04-27 17:40:59 +0300

    Prevent load patternfly and gluster in PPC
    Preventing loading repos with patternfly and gluster
    if we install on PPC/PPC64/PPC64LE architectures
    
    Change-Id: I575572bdfaccc57b4d63a2767ccb96fbc4d3f6fd
    Signed-off-by: Sagi Shnaidman <sshnaidm@redhat.com>

--- site/ovirt_jenkins_slave/manifests/engine_test_runner.pp
+++ site/ovirt_jenkins_slave/manifests/engine_test_runner.pp
@@ -95,20 +95,23 @@ class ovirt_jenkins_slave::engine_test_runner {
                   ensure => latest,
       }
 
-      ## Use a file resource instead of yumrepo because skip_if_unavailable is not supported yet
-      file {'/etc/yum.repos.d/gluster.repo':
-        ensure => present,
-        mode   => '0664',
-        owner  => 'root',
-        group  => 'root',
-        source => 'puppet:///modules/ovirt_jenkins_slave/gluster.epel.repo';
-      }
+      ## Use a file resource instead of yumrepo because skip_if_unavailable is
+      ## not supported yet
+      unless 'ppc' in $::architecture {
+        file { '/etc/yum.repos.d/gluster.repo':
+          ensure => present,
+          mode   => '0664',
+          owner  => 'root',
+          group  => 'root',
+          source => 'puppet:///modules/ovirt_jenkins_slave/gluster.epel.repo';
+        }
 
-      yumrepo{'patternfly':
-        descr    => 'Copr repo for patternfly1 owned by patternfly',
-        baseurl  => 'http://copr-be.cloud.fedoraproject.org/results/patternfly/patternfly1/epel-$releasever-$basearch/',
-        gpgcheck => 0,
-        enabled  => 1,
+        yumrepo{ 'patternfly':
+          descr    => 'Copr repo for patternfly1 owned by patternfly',
+          baseurl  => 'http://copr-be.cloud.fedoraproject.org/results/patternfly/patternfly1/epel-$releasever-$basearch/',
+          gpgcheck => 0,
+          enabled  => 1,
+        }
       }
     }
     default: {
