440751e8609f05484f6c5d11e6abfda7466a88ad
Max Kovgan <mvk@redhat.com>
2015-06-11 11:00:04 +0300

    rm users keys
    Change-Id: I68c476f0be6df05f73a938126025eaab9097ba20
    Signed-off-by: Max Kovgan <mvk@redhat.com>

--- site/ovirt_infra/manifests/user/knesenko.pp
+++
@@ -1,7 +0,0 @@
-# The user for Kiril Nesenko
-class ovirt_infra::user::knesenko($password = undef) {
-  ovirt_infra::user {'knesenko':
-    key      => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC3lARddzwVFQu8WPibzkCz830OjzTztEOUE6uejgsqUTbd15VmAlUhm2UFZnElUVHk1cwpr7KYFWEmzFRzJ8w79j3AgfGqauvIXPfClsEfqp3UPPGDok7HakXnGj6zV4ePNzHsQfn38DxwLblyR9c+S2O4R4sL62J2BhESvZGG5RfzlJPZghgkuh8jjlWDqN4jTYISMOaZ+jOueFLanEEzOuqct5QhyA1ToseWTfotXm40PYQlfx2OP9ub9Wevb1Sj0b832KccP+xrrePuRqgpJdg0PZZb/HknIND44k5gm+sMvm8mX6R6YfDPZNli76kCGi8hqiKnUroCe2niRe8x',
-    password => $password,
-  }
-}
--- site/ovirt_infra/manifests/user/obasan.pp
+++
@@ -1,7 +0,0 @@
-# The user for Ohad Basan
-class ovirt_infra::user::obasan($password = undef) {
-  ovirt_infra::user {'obasan':
-    key      => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDUHfDVi+l1Mv/qF7oThINAmSpGtkw+Mi8kPk3m8funyj+goa/thUq6rGfk4PvToetmMVZmLFJt+eljiX01cZPzihdKRVK4OE3KfX8xDAiF93m2NhOpoPi6N3GS9q8crs5jLhV9A3ciAZaXOF3xtrffBvXVmdBKcqQUvoLQeXIMDq4/XrpP4HYPvlzEx5w2s06j1oWhthOjYm7bugwumn6VCBLo32jHRnKnqZI68Fig22KJlkt23i90gVycBOyVW2qCWBQZTxbwwXE98ujUVyvjFnr8+7H7llHchj5VpcW63I1fjXyMrgUgakzTZ2RwtByAxxgWevDXP34/P/SFYmkD',
-    password => $password,
-  }
-}
