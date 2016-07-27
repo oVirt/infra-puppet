4d7ba96986ab067251141b5c42a8452401bc5a6a
Pavel Zhukov <pzhukov@redhat.com>
2016-07-27 12:13:40 +0200

    Change pzhukov's key to RSA
    Change-Id: I592121866f1cc2239842833e283d2478c7df8bb5
    Signed-off-by: Pavel Zhukov <pzhukov@redhat.com>

--- site/ovirt_infra/manifests/user/pzhukov.pp
+++ site/ovirt_infra/manifests/user/pzhukov.pp
@@ -1,7 +1,7 @@
 # The user for Pavel Zhukov
 class ovirt_infra::user::pzhukov($password = undef) {
   ovirt_infra::user { 'pzhukov':
-    key      => 'AAAAB3NzaC1kc3MAAACBAMebx93t5MedSKNju67yHFyg6Ih8MvVERjktPQz2Sx4xBZHqSC3O9+T+va6LyBENRO+49k5BDbRnXMh8dpRQJTtIGoA5Egp2P1E0k7t9+7YwTujw7F7RyCXuZN/nSJsMXtGNGzlNADDiFcknn297rOUjJtSSxtDkdrrzHeGJJ5MjAAAAFQDZ/BVtDxD6POoKr8qgh53PKkOw/QAAAIBpU/hsN7YrwVEuiJTUhSPthfx9CrXKegziUEA+QAr8vVlM/lkmGat4qqbu7VExmsj+l4or3qXkxAONdKYqi/WFUOzpoEmtmEax8R4Z6uvxCGEBrkWD5uKf4NARcbpYYISho791TFvN16p1BHYf2mmrvg9u9vl6GtuP9+0g4FPdMAAAAIEAsNTDtNd5N/ExcOlhHOFgRSByliFfU6IAZvHIpHocPcoM/OJqxE2wbIuhHn5PlCmZh9Bj8qpFxx1RGbkvuyQC+hHEbOfs05u/qwKi+gV4Tb8R+j4OoJVqoNqwdbUm3Zp3h4PcKJ1Lv+sL6aYnBch1WHfVgYahqnAxNhdLtOr2yB0',
+    key      => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDUBdY4lw+fLjhDyxWL6NmxfQ0lSXmsLCOqISkJVh6Z44n86q2UQw0j+aXYKgSdrPuc/jzOmXHyXMFF32RMI4fz6ekKdjYnCYxf7Q/ypoWTS5I3PBgj3YIFs3OLReUPBsp5tEkscYLZG0xMwFRHsuNTTjX+N18ur5aTXecfIZfGXB7JslqW5pd3fh1mr22A5R8FvUBZtJPchUC6/YYW5Jzc5+7bCSxCEz8cP88q1e5CBq6M1ZflKWhqjXpLyO54u1pn/S/HoXkcxkDFCUw0WlFW1rbYEJ2Ns0+HQJg6+ktiU/HwfPNyYTxYeQT0c1omCgCN9EtM1KX0UVR62gzlXqLP',
     password => $password,
   }
 }
