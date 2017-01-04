d3874312ea1e077bb707fe7003cbecef75384e2c
Barak Korren <bkorren@redhat.com>
2017-01-04 15:59:06 +0200

    Fix race condition in experimental cleanup script
    See Jira ticket for details.
    
    Change-Id: I0a45f7e41cfb7859223a6ded03388dd1c9a11fd6
    Jira-Ticket: https://ovirt-jira.atlassian.net/browse/OVIRT-1005
    Signed-off-by: Barak Korren <bkorren@redhat.com>

--- site/ovirt_resources/files/cleanup.sh
+++ site/ovirt_resources/files/cleanup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash -e
 
-
 find \
     /srv/resources/repos/ovirt/experimental/*/* \
     -maxdepth 0 \
     -type d \
     -not -name 'latest.*' \
+    -and \
+    -not -name 'latest' \
     -and \
     -mtime +1 \
     -exec rm -rf {} \;
