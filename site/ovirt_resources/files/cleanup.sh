95944753ae9f1f32ae5ca97a377202ec3ac07880
Daniel Belenky <dbelenky@redhat.com>
2017-01-24 14:12:15 +0200

    added locking on deploy.sh and start_testing_latest.sh
    https://ovirt-jira.atlassian.net/browse/OVIRT-899
    
    Change-Id: Iecafc51aa03a25a4a0a6cb1a9900256e7b36cab9
    Signed-off-by: Daniel Belenky <dbelenky@redhat.com>

--- site/ovirt_resources/files/cleanup.sh
+++ site/ovirt_resources/files/cleanup.sh
@@ -1,5 +1,23 @@
 #!/bin/bash -e
 
+MAX_LOCK_ATTEMPTS=120
+LOCK_WAIT_INTERVAL=5
+LOCK_PATH=$HOME/experimental_repo.lock
+
+wait_for_lock() {
+    for ((i = 0; i < $MAX_LOCK_ATTEMPTS; i++)); do
+        if (set -o noclobber; > $LOCK_PATH) 2> /dev/null; then
+            trap "rm -f '$LOCK_PATH'" EXIT
+            return
+        fi
+        sleep $LOCK_WAIT_INTERVAL
+    done
+    echo "Timed out waiting for lock" >&2
+    exit 1
+}
+
+wait_for_lock
+
 find \
     /srv/resources/repos/ovirt/experimental/*/* \
     -maxdepth 0 \
@@ -7,6 +25,8 @@ find \
     -not -name 'latest.*' \
     -and \
     -not -name 'latest' \
+    -and \
+    -not -name 'el7Server' \
     -and \
     -mtime +1 \
     -exec rm -rf {} \;
--- site/ovirt_resources/files/deploy.sh
+++ site/ovirt_resources/files/deploy.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
+
+MAX_LOCK_ATTEMPTS=120
+LOCK_WAIT_INTERVAL=5
+LOCK_PATH=$HOME/experimental_repo.lock
+
+wait_for_lock() {
+    for ((i = 0; i < $MAX_LOCK_ATTEMPTS; i++)); do
+        if (set -o noclobber; > $LOCK_PATH) 2> /dev/null; then
+            trap "rm -f '$LOCK_PATH'" EXIT
+            return
+        fi
+        sleep $LOCK_WAIT_INTERVAL
+    done
+    echo "Timed out waiting for lock" >&2
+    exit 1
+}
+
+wait_for_lock
+
 repoman \
     --verbose \
     --temp-dir generate-in-repo \
