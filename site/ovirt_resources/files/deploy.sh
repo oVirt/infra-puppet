4c70712e9ee1ea0544b5a4a5983a61b99f98d6ea
Gil Shinar <gshinar@redhat.com>
2017-02-01 18:13:57 +0200

    Added a sleep time of wait+1 before lock loop
    In order to avoid scenario of deploy jobs that are executing
    one after the other and by that not allowing new test jobs
    to acuire the lock
    
    https://ovirt-jira.atlassian.net/browse/OVIRT-899
    
    Change-Id: Ie52633b822f720189f6d9a3d30d972efbc370d48
    Signed-off-by: Gil Shinar <gshinar@redhat.com>

--- site/ovirt_resources/files/deploy.sh
+++ site/ovirt_resources/files/deploy.sh
@@ -2,9 +2,11 @@
 
 MAX_LOCK_ATTEMPTS=120
 LOCK_WAIT_INTERVAL=5
+WAIT_BEFOR_LOCK_INTERVAL=$((LOCK_WAIT_INTERVAL + 1))
 LOCK_PATH=$HOME/experimental_repo.lock
 
 wait_for_lock() {
+    sleep $WAIT_BEFOR_LOCK_INTERVAL
     for ((i = 0; i < $MAX_LOCK_ATTEMPTS; i++)); do
         if (set -o noclobber; > $LOCK_PATH) 2> /dev/null; then
             trap "rm -f '$LOCK_PATH'" EXIT
