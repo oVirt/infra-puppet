a30e0533609dafa6f032182562d6fc75f522a1e8
Ido Rosenzwig <irosenzw@redhat.com>
2017-09-27 17:06:42 +0300

    Add user for Ido Rosenzwig
    Change-Id: I2c70f69fbc3228d023a0bcd5bc33d89f71e1d83c
    Signed-off-by: Ido Rosenzwig <irosenzw@redhat.com>

--- site/ovirt_infra/manifests/user/irosenzw.pp
+++ site/ovirt_infra/manifests/user/irosenzw.pp
@@ -1,7 +1,7 @@
 # The user for Ido Rosenzwig
 class ovirt_infra::user::irosenzw($password = undef) {
   ovirt_infra::user { 'irosenzw':
-    key      => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDiwvY0GGKk34GHr5/Ui+C0qfNKLRgjOa1HqH072EO5Dr9JU35ySzNi+S+pRWTGtUFi9oUzPZJHkROKUV2E06XxkMrxp25JqB1dOjyvALy6oiKrmXMYVGymp9kzRVTBb6N3FM0cgfkeD/VCF+e5W4RYHEM33G6zfYfaEJe1dGwQFCi5NNcguQP9Jm/lyXpwm4gMgQUBXaKT3jVL/XSmzHoGpaP4SLFMEXu1hwR8/4Wma5MEXl4Jrn3h+xThjvOiy6bqmojV1JduiAUgbb9qVNHIQ9szjts0VP6En9DfFB9SF915ldzrtdxEEPcEfQ95bFnQxcUhcPCQvKvzUSeI8rf3',
+    key      => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC4U760ZdughzAVILQF1BmzGREudMzRUVg7MW902Vx7BmOD868Hf3c+kIxaqha0T2UFvWKK6iqzFQM7H4+uGPP1gnv/1xiaF/KOrX6IBUDa1Z6dAL80JNmsuEJzB1aAoJBXTCSJY2qMO+uPzrObEVF5CnEyAY9rTVF0g2ocN7eXJpv5hhvKlLY3m4TOrun2U/dKVIfFt1aJxPb96G0j754T6gupa8RUiHc2+K4zn1q+UYDFVHlly25pQKzO7l/iaxAibQ9Fv2yFcxR/c03QBlbeVv6BamBVvkG5P/K6890CIZ5s3yMe/ZfOT6PMtZj1DHm02/m51fxMRdFB8FfKM2np',
     password => $password,
   }
 }
