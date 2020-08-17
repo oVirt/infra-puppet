63a2bb633f4689ccb0a92cfab29db94fa66a55e1
Dusan Fodor <dfodor@redhat.com>
2020-08-17 17:37:33 +0200

    user key for dfodor refreshed
    Change-Id: Ia97a1c832dae6304d116beb2b5d799dc6fcd9522
    Signed-off-by: Dusan Fodor <dfodor@redhat.com>

--- site/ovirt_infra/manifests/user/dfodor.pp
+++ site/ovirt_infra/manifests/user/dfodor.pp
@@ -1,7 +1,7 @@
 # The user for Dusan
 class ovirt_infra::user::dfodor($password = undef) {
   ovirt_infra::user { 'dfodor':
-    key      => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCXiP8/T3HRZ6JhyDGcgsTtAR+2jv/m58eOgf/67FMR0KwgMmVYL3B8BnmPDVxiUvMpSXGhEQgKmRpNIEgrP70/VPRFl2IdO/2sl+DmKVG7PlVYtHGda0mrhCI8KsG8N631olveJwqlYnCXLFMDUGexaTBguwBTIB9uJtHd2kD20vf8QgmX7pshpxAVuXvIbU6SBJOEMCLFbM+Gj7+LqqV/rdN4COxADzAs4wtHdWCunst2JSsyCCQ4Lpdoc7w1z6GvPzv+yrAohK7gDcgnB/v6+grtOeeFOGFiawWtgkU594MIrZ/3cxogqM+ZRvaU1z8nQpGNDNuMr77Wm5fyiSa/',
+    key      => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC2J9ephq7Bd2rkMr+8CVD1n+eizQpk2TCVJf+yqXEra285vysYg5km7HqV/+650bLDG3PyqD4xVnP6woF1FYV7GenBSft98ww042wqNsFcOI6fyRpkQj+3ZLoRfdItkeowZ1s5183QJrmTu/XDCBEe80VQxFsuD4E19FBVfHcp2c9k2X2C8NhcVeGr1oPV48aiAjtL2mlKCYXD5WfzFhk0czwWzygUUGWHGoDA70tQr6w47u90n7yk2ptnJV8EeXdK8nb3ocVoDa2GOvymzoXySf/2Euu1wG3Y19t5Ymj97WGY+90ZVHOmc2AMx5iiSsYrHk93fWgujfg8unPJ/Hq5',
     password => $password,
   }
 }
