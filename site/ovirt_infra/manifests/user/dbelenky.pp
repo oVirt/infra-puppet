7b30a99e5f5082b1aea5ab098964cff0d6688461
Sandro Bonazzola <sbonazzo@redhat.com>
2021-01-20 13:01:20 +0100

    Disable infra users for inactive team members
    Daniel left the team a few months ago.
    
    Change-Id: I229fe59cd826027d6ce739aca8e1e7d80e013edf
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- site/ovirt_infra/manifests/user/dbelenky.pp
+++ site/ovirt_infra/manifests/user/dbelenky.pp
@@ -1,7 +1,6 @@
 # The user for Daniel Belenky (dbelenky)
 class ovirt_infra::user::dbelenky($password = undef) {
   ovirt_infra::user { 'dbelenky':
-    key      => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDGWNBWF2vLW1P+hi4hK7oFuyDKAtdHx4dgbLQiEY0QhCx6krUZCRXqE++wtanlzuFP2n7klFARst8GhBqjEtdG3xFIsmcb89VqaRvAMR/XO0nPOfFnvHQxUDxJVRxbrxoQiEc5mjfUeVTu3e5V/gKERPNIcd+yj6qJ4Zw3/fj/yfpJ4kUShzAJxsh1nbcxbwl8wEcjO/4K9Pvuje5m3QNl7QiuWrWOUzrWYotV8m0oUeqIdfhZlLzPj19I38Bgbs9+Iet21yOzcokovswruULb+WqL73BbtRBAITUErMSK8Ks5858gP+kf5cDWGaCRUazaMQxNvrBpql/eBVJNviS5',
-    password => $password,
+    ensure => absent,
   }
 }
