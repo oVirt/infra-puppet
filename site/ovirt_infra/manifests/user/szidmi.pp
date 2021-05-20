166a2103decb0112b6e73121f53422d8597fb80e
Evgheni Dereveanchin <ederevea@redhat.com>
2021-05-20 16:39:20 +0200

    Disable infra user for Shlomi
    Shlomi is no longer with the infra team,
    this patch disables the user account.
    
    Signed-off-by: Evgheni Dereveanchin <ederevea@redhat.com>
    Change-Id: I3a76ec4e40f16e852ffe34e84e20d2a1966ca305

--- site/ovirt_infra/manifests/user/szidmi.pp
+++ site/ovirt_infra/manifests/user/szidmi.pp
@@ -1,7 +1,6 @@
 # The user for Shlomi Zidmi
 class ovirt_infra::user::szidmi($password = undef) {
   ovirt_infra::user {'szidmi':
-    key      => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDNTrgWpi5d8HXZvF3kObOKmp/ShsYRBKLd0nerZLKMUoL6+KCBKYznIXJtUytE3qd/loDAOaYzA1bqLQMVYYpuCKBfIn4bG0c5vN+4bcoSbwCsELnuC+RShnyJmk/Oqe05fdFEhirnt4Xrm9YexnALWbbbiIIzfK+KUeJEMQ9KV8qksCKoM23K87PTSsi/6elqaxDhlpDNb7YArXCnbxmuWjLV7FvWMAziVydaJHWnnLa5mf1LyiNpkFNZG/w+2bymRUFWKWDG4PQK93cXFf13kHhWCjGxwGRF72JSTJ54K7Mygk79omd0yqqdTxJWScrrF5lzKpyQVyXLyL0HAst1',
-    password => $password,
+    ensure => absent,
   }
 }
