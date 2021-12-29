ff74381803c9681a2298fbc2e83f19e59e5fd02e
Ehud Yonasi <eyonasi@redhat.com>
2021-12-29 10:17:22 +0200

    update public key for eyonasi.
    Signed-off-by: Ehud Yonasi <eyonasi@redhat.com>
    Change-Id: Iec611eef20d061b49ae7e30076afd471f7c120fe

--- site/ovirt_infra/manifests/user/eyonasi.pp
+++ site/ovirt_infra/manifests/user/eyonasi.pp
@@ -1,7 +1,7 @@
 # The user for Ehud Yonasi
 class ovirt_infra::user::eyonasi($password = undef) {
   ovirt_infra::user {'eyonasi':
-    key      => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCq1PlLWFYfn0LtD4GdDQRMFaCizbbqoD0gE6NYDHy4tIGrJ7HbKtzRweMHBh7SHcOmOMdvhEHx+1wuzYJ2cZCuRQebtqYbXYvvVbRAs8oOMXr23I0+qqpCcjZeNSKdqvIXgruuOPug4nRcyg3LxZ3xnsgQnxSIm9BQNjn2tvN6aAtAf9HmYW32qtE042pzJYRh8qQerKzoLpd/zd9hMccNeRCdxAXxHDZJO2GpfSbi1A7IKmwKcWS+V2pvuI6I1JQr+WJmna0T7VZdLQVAQq7RjPq0Kv9xFh0Ot+LyPX0Fdk/EHXW11JriyMNIXSjFX586UhS9At/UVZ6+utv3sRtT',
+    key      => 'AAAAB3NzaC1yc2EAAAADAQABAAABgQDqmYYwX/CJNkPYGqhy+EMg9mBaRsoUMZHqZjpMQiSNLkZ2vj034bqnEzaHf/+CF73mVyJLxLirQpaI4MtMJnaET0wLzW1Gg0VTEClXzn3J9K+0+UGAHKFCD2Hw3hEoaMhimzpSKFgwHRlVZMsOffJz8g5q+4PYUrxw6pTycqABINCvwADsbYCBw6xUCACVWwIqCCICR7rvm2ATfCIXj4YEXV4E+JKUlswVIuzhD++9x/3uYBJI9pCkg7YrzFPx0L35izktu7jnbhRUSAwzmuoNI8F8wjhhtgIXRXnKTBTA1J07r0Ei6MperVWelxLx5pZNSBOUCbpr71XLS5J1fQ0jcJf885zcQON9BiIdPSw3p9rwgXgXUCYO71zuoVpdH4lgwty0zhAK7j6vs9kNfiecSj1+rqahE7euFb0OHGVoGngr8Vj3hmo9Ns26HLVM2xOy7rE0a5kUBDYIWvB6jm9LtlO4BzD6ZOIjMlB4rjLVcz6YK4h6bX1pUKFuAxHXBNM=',
     password => $password,
   }
 }
